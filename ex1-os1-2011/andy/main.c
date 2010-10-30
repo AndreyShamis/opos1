1546659c750c2048a1f2444799e52a767920a761
lolnik@gmail.com <lolnik@gmail.com@4de3f0e1-184e-52b5-56bf-05c33a74d31c>
2010-10-30 13:30:42 +0000

    ,,,

--- ex1-os1-2011/andy/main.c
+++ ex1-os1-2011/andy/main.c
@@ -9,28 +9,46 @@
 
 //------------------------- including section ---------------------------------
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //-------------------------  Const section ------------------------------------
 //const int MAX_STR_LEN 5;
 
-void sort_by_name();
+//------------------------------ Sort By NAME ---------------------------------
+void sort_by_name(char **data)
+{
+	int high;
+	
+	high = sizeof(data);
+	printf("size: %d \n",high);
+	printf("By name\n");
+
+}
+
+
 
 
 void sort_by_id();
 
-
+int getID(char *str);
+int cmpName(char *name1,char *name2);
 //--------------  Prototypes section ------------------------------------------
 
 //------------------------- Main section --------------------------------------
 int main(int argc, char *argv[])
 {
 	
-	char key[5];		//	Variable for menu
-	char buf[20];		//	Variable for In data
+	char key[5];		//	Variable for menu		// TODO
+	//char buf[20];		//	Variable for In data	// TODO
+	
+	char data[10][20];
 	
 	FILE *fRead;		//	Var for red file
 	FILE *fWrite;		// 	Var for write file
 		
+	int ind=0;
+	
 	if(argc == 3 )
 	{
 
@@ -40,13 +58,22 @@ int main(int argc, char *argv[])
 		if(fRead == NULL)
 			printf("Can`t read file %s \n", argv[1]);
 		else
-			while(fgets(buf,20,fRead) != NULL)
-				printf("%s \n",buf);	
-	
-		gets(key);
+			while(fgets(data[ind],20,fRead) != NULL)	// TODO CONST
+			{
+			
+				//fputs(buf,data[ind]);
+				printf("%s \n",data[ind]);	
+				
+				//printf("---- %d \n",getID(data[ind]));
+				
+				ind++;
+			}
+		
+		
+		fgets(key,4,stdin);			// TODO Const
 	
 		if(!strcmp(key,"name"))
-			sort_by_name();
+			sort_by_name(data);
 		else if(!strcmp(key,"id"))
 			sort_by_id();
 	
@@ -62,19 +89,87 @@ int main(int argc, char *argv[])
 	return(1);
 }
 
-//------------------------------ Sort By NAME ---------------------------------
-void sort_by_name()
-{
-	printf("By name\n");
-
-}
-
-
-
 //------------------------------ Sort By ID -----------------------------------
 void sort_by_id()
 {
 	printf("By id\n");
 
+}
+
+int getID(char *str)
+{
+	int count;
+	char id[strlen(str)];
+	int result;
+	
+	for(count=0;count<strlen(str);count++)
+	{
+		if(str[count] == ' ')
+		{
+			strncpy(id,str,count+1);
+			break;
+		}
+			
+	}
+	
+	result= atoi(id);
+
+	printf("%d \n",result);
+
+	return(result);
+}
+
+int cmpName(char *name1,char *name2)
+{
+	int count;
+	
+	int name1_len;
+	int name2_len;
+	
+	char name1_str[strlen(name1)];
+	char name2_str[strlen(name2)];
+	
+	for(count=0;count<strlen(name1);count++)
+	{
+		if(name1_str[count] == ' ')
+		{
+			//strncpy(id,str,count+1);
+			
+			name1_len = count-1;
+			strncpy(name1_str,name1,count-1);
+
+			break;
+		}
+			
+	}
+	
+	
+	for(count=0;count<strlen(name2);count++)
+	{
+		if(name2_str[count] == ' ')
+		{
+			//strncpy(id,str,count+1);
+			strncpy(name1_str,name1,count-1);
+			
+			name2_len = count-1;
+			break;
+		}
+			
+	}
+	
+	if(strcmp(name1_str,name2_str))
+	{
+		printf("%s \n",name1_str);
+		printf("%s \n",name2_str);
+		return(1);
+	}
+	else
+	{
+		printf("%s \n",name2_str);
+		printf("%s \n",name1_str);
+		return(0);
+	}
+
+	
 }
 
