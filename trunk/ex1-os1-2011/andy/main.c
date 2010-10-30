5dbf69214ec47b9c2e621dc8bf7598e2151b3990
lolnik@gmail.com <lolnik@gmail.com@4de3f0e1-184e-52b5-56bf-05c33a74d31c>
2010-10-30 17:15:42 +0000

    ,,,

--- trunk/ex1-os1-2011/andy/main.c
+++ trunk/ex1-os1-2011/andy/main.c
@@ -16,20 +16,121 @@
 //const int MAX_STR_LEN 5;
 
 //------------------------------ Sort By NAME ---------------------------------
-void sort_by_name(char **data)
+void sort_by_name()
 {
-	int high;
+	//int high;
 	
-	high = sizeof(data);
-	printf("size: %d \n",high);
+	//high = sizeof(data);
+	//printf("size: %d \n",high);
 	printf("By name\n");
 
 }
 
-
+char *all_one_arr(int to, char *grades);
 
 
 void sort_by_id();
+
+
+char **create_Cell(char **data,int len)
+{
+
+	char **new_data = (char **)malloc(sizeof(char)*len);
+	//int count;
+	
+	if(new_data == NULL)
+		printf("Can`t allocate more memroy");
+		
+	printf("aa: %d",sizeof(char)*len);
+	
+	//for(count=0;count<len-1;count++)
+	//{
+		new_data = data;
+	//}
+	free(data);
+	return(new_data);
+	
+}
+
+//-------------------- ALLOCATE ** ARRAY --------------------------------------
+// Function to allocate memory for pragram data
+// geting lower and upped index to allocate
+// returning new data
+char **all_two_arr(int to, char **stud)
+{
+    char **new_stud = NULL;      // create new pointer-pointer
+
+    new_stud = (char**)malloc(sizeof(char)*to) ;   // alloc memory
+
+    if(new_stud == NULL)        // check if have memory
+    {
+        //cout << "Error: alloc one array\n";
+        //exit(EXIT_FAILURE);     // if not go away
+    	printf("Error ----- all\n");
+    }
+
+	int i;
+	int x;
+
+    for(i=0;i<to-1;i++)
+    {
+        //if(i < to-1)
+        //{
+            // start copy data from previous cellsd
+            new_stud[i] = NULL; // set be NULL
+            // try allocate more memory
+            new_stud[i] = all_one_arr(strlen(stud[i])+1,stud[i]);
+            // copy previous data
+            
+            // copy all rows from 2
+
+            //free(stud[i]);                  // free memory
+
+        //}
+        //else
+        //{
+        //    new_stud[i] = NULL;                 // set be NULL
+        //    // allocate more memory for new cells
+        //    new_stud[i] = all_one_arr(,new_stud[i]);
+            
+        //}
+	    
+	    //free(stud);                 // global free memory
+     }   
+    return(new_stud);                           // retun the data
+
+}
+
+//---------------------- MEMORY ALLOCATE --------------------------------------
+// allocate memory only for rows/
+// function get upper index needed to allocate
+// act it is for if use trying allocate new array
+// return pointer to new data
+char *all_one_arr(int to, char *grades)
+{
+    char *arr ;                          // pointer for new data
+
+    arr = (char*)malloc(sizeof(char)*to);  // try get memory
+
+    if(arr == NULL)                     // check if geting success
+    {
+        //cout << "Error: alloc one array\n"; // print error
+        //exit(EXIT_FAILURE);                 // if not exit
+    	printf("Error aloc 1");
+    }
+
+	int i;
+    // start work whith data
+    for(i=0;i<to;i++)
+       	arr[i] = grades[i]; // copy old data
+        
+    // trying to delete old data
+    //if(grades != NULL)
+       //free(grades);        // deleting
+
+    return(arr);                // returning
+
+}
 
 int getID(char *str);
 int cmpName(char *name1,char *name2);
@@ -43,6 +144,8 @@ int main(int argc, char *argv[])
 	//char buf[20];		//	Variable for In data	// TODO
 	
 	char data[10][20];
+	
+	char **datap;
 	
 	FILE *fRead;		//	Var for red file
 	FILE *fWrite;		// 	Var for write file
@@ -61,19 +164,22 @@ int main(int argc, char *argv[])
 			while(fgets(data[ind],20,fRead) != NULL)	// TODO CONST
 			{
 			
+				ind++;
+//				datap = create_Cell(datap,ind);
+				datap = all_two_arr(ind,datap); 			
 				//fputs(buf,data[ind]);
-				printf("%s \n",data[ind]);	
+				printf("%s \n",data[ind-1]);	
 				
 				//printf("---- %d \n",getID(data[ind]));
 				
-				ind++;
+				
 			}
 		
 		
 		fgets(key,4,stdin);			// TODO Const
 	
 		if(!strcmp(key,"name"))
-			sort_by_name(data);
+			sort_by_name();
 		else if(!strcmp(key,"id"))
 			sort_by_id();
 	
