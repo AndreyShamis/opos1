e71c8a6e370536bebde19ef4cabfe5e404f694e9
lolnik@gmail.com <lolnik@gmail.com@4de3f0e1-184e-52b5-56bf-05c33a74d31c>
2010-11-14 20:05:27 +0000

    dohuya define

--- trunk/ex2-os1-2011/ilia/utils.h
+++ trunk/ex2-os1-2011/ilia/utils.h
@@ -5,8 +5,23 @@
 #define MIN_STR_LEN		2		//	min string lentgh name+space+id
 #define NUMBER_PARAM	3		//	number of parameters needed
 #define NUMBER_SONS		2		//	numbers of sons
+#define MAX_FILE_NAME 	100		//	max len of file name
+
+//	sort types
+#define SORT_BY_ID		"id"	//	const for set type of sort needed
+#define SORT_BY_NAME	"name"	//	const for set type of sort needed
+//	files names
+#define ID_FILE_NAME	"id.out"	//	file name for outputs id`s
+#define NAME_FILE_NAME	"name.out"	//	file name for output names
+//	file open types
+#define FILE_READ		"r"		//	type of open file -	for read 
+#define FILE_WRITE		"w"		//	type of open file - for write
+
+//	function for exec
+#define CAT_FILE		"cat"	//	function to read whith cat
 
 
+//------------------ DEFINE ENUM SECTION --------------------------------------
 #ifndef UTILS_SEC
 #define UTILS_SEC
 
