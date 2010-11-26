edc074ad37c811878aa902cc345bf486f43ab7df
lolnik@gmail.com <lolnik@gmail.com@4de3f0e1-184e-52b5-56bf-05c33a74d31c>
2010-11-26 16:14:03 +0000

    & -dosnt work; before wait4

--- ex3-os1-2011/main.c
+++ ex3-os1-2011/main.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "mem.h"
 
 
 
-#define MAX_INPUT_LEN 80
+#define MAX_INPUT_LEN 30
+
+
+
+
 
 void print_arr_t(char **data,const int size)
 {
 	int i=0,x=0;
 
 
-		//printf("%d \n",strlen(data[i]));
+	printf("PRINT SIZE: %d\n",size);
 	
 	for(i=0;i<size;i++)
 	{
-
-		for(x=0;x<strlen(data[i]);x++)
-		{	
-			if(data[i][x] == ' ')
-				printf("\r\t\t\t\t");
-				
-			printf("%c",data[i][x]);
+		if(data[i] != NULL)
+		{
+			for(x=0;x<strlen(data[i]);x++)
+			{	
+				printf("%c.",data[i][x]);
 			
+			}
 		}
 		printf("\n");
 	}
@@ -41,10 +46,12 @@ char *substr(const char *string, const int start,const int len)
 	
 	if(len > string_len || len <0)
 		len_self = string_len - start - 1;
+	else if (len == 0)
+		return(NULL);
 	else
 		len_self = len;
 
-	printf("Start: %d , Len: %d , SELF LEN: %d\n",start,len,len_self);
+	//printf("Start: %d , Len: %d , SELF LEN: %d; Used: %d\n",start,len,len_self,len_self+1);
 		
 	temp = alloc_string(len_self+1);
 	
@@ -58,6 +65,20 @@ char *substr(const char *string, const int start,const int len)
 }
 
 
+char **addTostr(char **arr,int *size)
+{
+	char 	**temp = NULL;
+	
+	temp = alloc_cell((*size)+1);
+	copy_arr(temp,arr,*size);
+	free(arr);
+	temp[*size] = (char *) NULL;
+	(*size)++;
+	
+	return(temp);
+	
+	
+}
 char **commandArr(const char input[], int *size)
 {
 	char **temp			=	NULL;
@@ -69,29 +90,30 @@ char **commandArr(const char input[], int *size)
 	
 	for(sit=0;sit<MAX_INPUT_LEN;sit++)
 	{
-	
-		if((input[sit] == ' ' || input[sit] == '\0'))
-		{
-			
-			temp = alloc_cell(space_counter+1);
-			//printf("%s \n",substr(input,string_start,sit-string_start));
-			
+		if(input[sit] == ' ' || input[sit] == '\0')
+		{			
 			new_string =substr(input,string_start,sit-string_start);
 			
-			if(return_temp != NULL)
+			if(new_string != NULL)
 			{
-				copy_arr(temp,return_temp,space_counter);
-				free(return_temp);
-			}
 			
-			temp[space_counter] = new_string;
+				temp = alloc_cell(space_counter+1);
+		
+				if(return_temp != NULL)
+				{
+					copy_arr(temp,return_temp,space_counter);
+					free(return_temp);
+				}
+			
+				temp[space_counter] = new_string;
 			
 
-			string_start = sit+1;	//	set left from space next char
+				string_start = sit+1;	//	set left from space next char
 
-			space_counter++;		//	counter of strings counted
+				space_counter++;		//	counter of strings counted
+				return_temp = temp;
+			}			
 			
-			return_temp = temp;
 		}
 
 	
@@ -99,7 +121,7 @@ char **commandArr(const char input[], int *size)
 			break;
 	}
 	
-	printf("Found : %d .\n", space_counter);
+	//printf("Found : %d .\n", space_counter);
 	
 	(*size) = space_counter;
 	
@@ -107,22 +129,134 @@ char **commandArr(const char input[], int *size)
 	
 }
 
-int main()
+int multiTask(char **data, const int size)
 {
+	int counter=0;
 	
+	for(counter = 0;counter < size;counter++)
+		if(strlen(data[counter]) == 1 && data[counter][0] == '&')
+		{
+			data[counter][0] = ' ';
+			return(1);
+		}
+		
+	return(0);
+	
+}
+
+void catch_chld(const int num)
+{
+	int sys_time = 0;
+	int usr_time = 0;
+	int exit_stat= 0;
+	printf("CHILD NUMBER:%d\t",num);
+	
+	printf("ppid %d\t", getppid());
+	printf("pid %d\n", getpid());
+	
+	printf("%d ,%d ,%d\n",sys_time,usr_time,exit_stat);
+}
+
+void del_new_line(char *string)
+{
+	int str_len	=	0;			//	string len variable		
+	//	get line of string
+	str_len = strlen(string);
+	//	put null terminated symbol to \n
+	
+	if(str_len > 0)
+		string[str_len-1] = '\0';
+		
+}
+
+
+void getstring(char *input,const int max_size)
+{
+	if(fgets(input,max_size,stdin) != NULL)
+		del_new_line(input);
+	else
+		input = NULL;	
+}
+
+void exec(char **vector_param,const int size)
+{
+	int exec_stat = 0;
+	
+	exec_stat = execvp(vector_param[0],vector_param); 
+
+	free_arr(vector_param,size);
+		
+	if(exec_stat)
+		exit(EXIT_FAILURE);
+	else
+		exit(EXIT_SUCCESS);
+
+}
+
+void setHendlerOptions()
+{
+	//	handler block 
+	struct sigaction act;
+	act.sa_handler = catch_chld;
+	sigfillset(&act.sa_mask);
+	act.sa_flags = 0; 				//	do every time
+	sigaction(SIGCHLD,&act,NULL);
+
+
+}
+
+int main()
+{	
 	char input[MAX_INPUT_LEN];
 	int size=0;	
 	char **vector_param = NULL;
-	
-	fgets(input,MAX_INPUT_LEN,stdin);
-	
-	vector_param = commandArr(input,&size);
-	
-	print_arr_t(vector_param,size);
+	int multi_task = 0;
+	pid_t status ;
 	
 	
-	free_arr(vector_param,size);
+	getstring(input,MAX_INPUT_LEN);
+	
+	while(input != NULL)
+	{
+	
+		if(!strcmp(input,"exit"))
+			break;
+		else if(!strcmp(input,""))
+		{
+			getstring(input,MAX_INPUT_LEN);
+			continue;
 		
+		}
+		//	covert command line to array
+		vector_param = commandArr(input,&size);
+		
+		//	check if have & in array/ remove them
+		multi_task = multiTask(vector_param,size);
+		//	add to array NULL on end of array
+		vector_param = addTostr(vector_param,&size);		
+		
+		
+		status = fork();
+
+		
+		if(fork <0)
+			exit(EXIT_FAILURE);
+		else if(status == 0)
+			exec(vector_param,size);
+		else if(status > 0)
+		{
+			if(multi_task)
+				printf("\nEnter to MULTI TASK MODE\n");
+			else
+				wait4(&status);
+			free_arr(vector_param,size);
+		}
+		
+		getstring(input,MAX_INPUT_LEN);
+				
+	}	
+
+	puts("Bye-Bye");		
 	return(EXIT_SUCCESS);
 }
 
